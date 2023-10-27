7f06a59d92f0c610ccd5441872ab16c9c650ee96
Vidyarani <vidyaranigrao@gmail.com>
2023-10-27 14:57:05 +1100

    Initial Commit

---
+++ DataScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataScanner
+//
+//  Created by Vidyarani Balakrishna on 27/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
