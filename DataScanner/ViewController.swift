3ded91bda623f966ee864a0eb8e13fb8340baba0
Vidyarani <vidyaranigrao@gmail.com>
2023-10-30 10:05:02 +1100

    Initial commit

--- DataScanner/ViewController.swift
+++ DataScanner/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import VisionKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    private let dataScannerViewController = DataScannerViewController(recognizedDataTypes: [.text(), .barcode(symbologies: [.qr])],
+                                                                      qualityLevel: .balanced,
+                                                                      recognizesMultipleItems: false,
+                                                                      isHighFrameRateTrackingEnabled: true,
+                                                                      isPinchToZoomEnabled: true,
+                                                                      isGuidanceEnabled: true,
+                                                                      isHighlightingEnabled: true)
+    
+    private var scannerAvailable: Bool { DataScannerViewController.isSupported && DataScannerViewController.isAvailable }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dataScannerViewController.delegate = self
     }
 
-
+    @IBAction func startScan(_ sender: Any) {
+        if scannerAvailable {
+            present(dataScannerViewController, animated: true)
+            try? dataScannerViewController.startScanning()
+        }
+    }
+    
 }
 
+extension ViewController: DataScannerViewControllerDelegate {
+    func dataScanner(_ dataScanner: DataScannerViewController, becameUnavailableWithError error: DataScannerViewController.ScanningUnavailable) {
+        print("The scanner became unavailable")
+    }
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+        guard let item = allItems.first else {
+            infoLabel.text = "Not able to scan"
+            dataScannerViewController.dismiss(animated: true)
+            return
+        }
+        switch item {
+            case .text(let text):
+                print(text.observation)
+                print(text.transcript)
+                infoLabel.text = text.transcript
+            case .barcode(let code):
+                let output = code.payloadStringValue ?? "barcode not found"
+                print(output)
+                infoLabel.text = output
+            default:
+                break
+        }
+        dataScannerViewController.dismiss(animated: true)
+    }
+}
